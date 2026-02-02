fdca0cfd594b0c6b27cc647ab3fb17eb68c0108e
Mar <marmoralespalop@gmail.com>
2026-02-02 01:02:17 +0100

    creacionDao

--- src/main/java/dao/AlumnoGrupoDAO.java
+++ src/main/java/dao/AlumnoGrupoDAO.java
@@ -1,9 +1,22 @@
 package dao;
 
+import java.util.List;
+
 import model.Alumno;
+import model.Grupo;
 
 public interface AlumnoGrupoDAO {
+	
 	int alumno_add(Alumno al);
 	
 	Alumno getByNia (int nia);
+	
+	List <Alumno> getAllAlumnos(); //datos del alumno y de su grupo
+	
+	int grupo_add(Grupo gr);
+	
+	void delete_alumno(int nia);
+	
+	void delete_alumnos_curso(int nia); //borrar los alumnos de un curso entero
+	
 }
---
+++ src/main/java/dao/AlumnoGrupoDAOImp.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.util.List;
+
+import model.Alumno;
+import model.Grupo;
+
+public class AlumnoGrupoDAOImp implements AlumnoGrupoDAO {
+
+	@Override
+	public int alumno_add(Alumno al) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Alumno getByNia(int nia) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Alumno> getAllAlumnos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int grupo_add(Grupo gr) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete_alumno(int nia) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete_alumnos_curso(int nia) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
